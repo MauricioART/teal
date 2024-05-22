2b63391e874b527a58bdac6906ae1ed66580b804
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-05-22 14:14:22 -0600

    feat(Root): Adding basic functionality to arrow buttons

--- app/learn/(overview)/page.tsx
+++ app/learn/(overview)/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, Suspense } from "react";
+import { useState, Suspense, ChangeEvent} from "react";
 import Card from "@/app/ui/learn/card";
 import Image from "next/image";
 import { ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, ArrowUpIcon } from '@heroicons/react/24/outline';
@@ -15,25 +15,33 @@ export default  function Page() {
   const [down, setDown] = useState(false);
   const [left, setLeft] = useState(false);
 
-  function handleClick(){
-    console.log("I have been clicked.")
+  function handleClickUp(){
     setUp(true);
+  }
+  function handleClickDown(){
+    setDown(true);
+  }
+  function handleClickLeft(){
+    setLeft(true);
+  }
+  function handleClickRight(){
+    setRight(true);
   }
   
   return (
     <main className="flex h-full px-0">
       <div className="flex justify-center items-center w-4/5 h-full relative">
-        <div className="absolute w-8 top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2" onClick={handleClick}>
-          <ArrowUpIcon />easy
+        <div className="absolute w-auto flex flex-col items-center top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 " onClick={handleClickUp}>
+          <ArrowUpIcon className="w-8"/><p>Normal</p>
         </div>
-        <div className="absolute w-8 right-1/4 top-1/2 transform -translate-x-1/2 -translate-y-1/2" onClick={handleClick}>
-          <ArrowRightIcon /> Normal
+        <div className="absolute w-auto flex flex-col items-center right-1/4 top-1/2 transform -translate-x-1/2 -translate-y-1/2" onClick={handleClickRight}>
+          <ArrowRightIcon className="w-8"/> <p>Hard</p>
         </div>
-        <div className="absolute w-8 left-1/4 top-1/2 transform translate-x-1/3 -translate-y-1/2 " onClick={handleClick}>
-          <ArrowLeftIcon /> Harder
+        <div className="absolute w-auto flex flex-col items-center left-1/4 top-1/2 transform translate-x-1/3 -translate-y-1/2 " onClick={handleClickLeft}>
+          <ArrowLeftIcon className="w-8"/> <p>easy</p>
         </div>
-        <div className="absolute w-8 bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/4" onClick={handleClick}>
-          <ArrowDownIcon /> Hard
+        <div className="absolute w-auto flex flex-col items-center  bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/4" onClick={handleClickDown}>
+          <ArrowDownIcon className="w-8"/> <p>Challenging</p>
         </div>
         
         <Card question={adverse} answer={["jhduiewewe","dfnuief","fdsneufis","sfesfsef"]} cardType={2} up={up} right={right} down={down} left={left}/>
