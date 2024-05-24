0dbfff16ebb3213e9e62621585a9643bf389e657
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-05-23 19:38:30 -0600

    feat(Root): Changing the layout of learn page

--- app/learn/(overview)/page.tsx
+++ app/learn/(overview)/page.tsx
@@ -1,15 +1,15 @@
 'use client';
-import { useState, Suspense, ChangeEvent} from "react";
+import { useState, Suspense, ChangeEvent, useEffect} from "react";
 import Card from "@/app/ui/learn/card";
 import Image from "next/image";
 import { ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, ArrowUpIcon } from '@heroicons/react/24/outline';
 import clsx from "clsx";
+import {Card as cardType} from "@/app/lib/definitions";
 
 
 export default  function Page() {
 
-  const adverse = "Adverse";
-  const reverse = "Reverse";
+  const adverse = "¿Qué tipos de celulas existen?";
   let cardsCount = 0;
 
   const [up, setUp] = useState(false);
@@ -19,25 +19,25 @@ export default  function Page() {
   const [enableArrowBtns,setEnableArrowBtns] = useState(false);
 
   const upArrowBtnClasses = clsx(
-    'absolute w-auto flex flex-col items-center top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 ',
+    ' w-auto flex flex-col items-center opacity-0 ',
     {
       'animateFadeIn': enableArrowBtns,
     }
   );
   const downArrowBtnClasses = clsx(
-    'absolute w-auto flex flex-col items-center bottom-0 left-1/2 transform -translate-x-1/2 -translate-y-1/4 opacity-0',
+    ' w-auto flex flex-col items-center opacity-0',
     {
       'animateFadeIn': enableArrowBtns,
     }
   );
   const leftArrowBtnClasses = clsx(
-    'absolute w-auto flex flex-col items-center left-1/4 top-1/2 transform translate-x-1/3 -translate-y-1/2 opacity-0',
+    ' w-auto flex flex-col items-center opacity-0',
     {
       'animateFadeIn': enableArrowBtns,
     }
   );
   const rightArrowBtnClasses = clsx(
-    'absolute w-auto flex flex-col items-center right-1/4 top-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0',
+    ' w-auto flex flex-col items-center opacity-0',
     {
       'animateFadeIn': enableArrowBtns,
     }
@@ -62,22 +62,27 @@ export default  function Page() {
   
   return (
     <main className="flex h-full px-0">
-      <div className="flex justify-center items-center w-4/5 h-full relative">
-        <div className={upArrowBtnClasses}  onClick={handleClickUp}>
-          <ArrowUpIcon className="w-8"/><p>Normal</p>
-        </div>
-        <div className={rightArrowBtnClasses} onClick={handleClickRight}>
-          <ArrowRightIcon className="w-8"/> <p>Hard</p>
-        </div>
-        <div className={leftArrowBtnClasses} onClick={handleClickLeft}>
-          <ArrowLeftIcon className="w-8"/> <p>easy</p>
-        </div>
-        <div className={downArrowBtnClasses} onClick={handleClickDown}>
-          <ArrowDownIcon className="w-8"/> <p>Challenging</p>
-        </div>
-        
-        <Card key={cardsCount++} question={adverse} answer={"Existen dos tipos principales de células: las células procariotas y las células eucariotas. Las células procariotas."} options={["jhduiewewe","dfnuief","fdsneufis","sfesfsef"]} cardType={1} up={up} right={right} down={down} left={left} setEnableArrowBtns={setEnableArrowBtns}/>
+      <div className="flex w-4/5 h-full">
 
+        <div className="flex justify-end items-center flex-grow">
+          <div className={leftArrowBtnClasses} onClick={handleClickLeft}>
+            <ArrowLeftIcon className="w-8"/> <p>easy</p>
+          </div>
+        </div>
+        <div className="flex flex-col items-center basis-auto flex-grow flex-shrink">
+          <div className={upArrowBtnClasses}  onClick={handleClickUp}>
+            <ArrowUpIcon className="w-8"/><p>Normal</p>
+          </div>
+          <Card key={cardsCount++} question={adverse} answer={"Existen dos tipos principales de células: las células procariotas y las células eucariotas. Las células procariotas."} options={["jhduiewewe","dfnuief","fdsneufis","sfesfsef"]} cardType={2} up={up} right={right} down={down} left={left} setEnableArrowBtns={setEnableArrowBtns}/>
+          <div className={downArrowBtnClasses} onClick={handleClickDown}>
+            <ArrowDownIcon className="w-8"/> <p>Challenging</p>
+          </div>
+        </div>
+        <div className="flex justify-start items-center flex-grow">  
+          <div className={rightArrowBtnClasses} onClick={handleClickRight}>
+            <ArrowRightIcon className="w-8"/> <p>Hard</p>
+          </div>
+        </div>  
       </div>
       <div className="w-1/5 h-full ">
       </div>
