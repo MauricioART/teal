2634a477fef96da8f26069c53c9ab391bf8be8e4
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-05-23 19:37:37 -0600

    feat(Root): Changing data types definitions of Deck, User and Card

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,18 +1,20 @@
-type cardContent = {
-    text: string | null;
-    image: Blob | null;
-}
 
-export type CardType = {
-    adverse: cardContent;
-    reverse: cardContent;
-}
 export type Deck = {
     name: string;
-
+    owner: User;
+    cards: Card[];
 }
 
 export type User = {
+    user_id: number;
     nickName: string;
     description: string;
+}
+export type Card = {
+    question: string;
+    image?: Blob;
+    answer: string | number;
+    options?: string[];
+    cardType: 0 | 1 | 2;
+    priority: number;
 }
