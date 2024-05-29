a8a0959b07352f49fb3c4bda204d7e10399ce22e
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-05-28 18:06:40 -0600

    feat(Root): Adding properties to Deck and User data types
    Adding a string to Deck datatype to store a short description and a Blob for a cover image. Adding
    
    an string for the email in User data type.

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,17 +2,19 @@
 export type Deck = {
     name: string;
     owner: User;
+    description: string;
+    coverImage: Blob;
     cards: Card[];
 }
 
 export type User = {
     user_id: number;
     nickName: string;
-    description: string;
+    email: string;
 }
+
 export type Card = {
     question: string;
-    image?: Blob;
     answer: string | number;
     options?: string[];
     cardType: 0 | 1 | 2;
