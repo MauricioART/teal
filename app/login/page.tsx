66d9c8309952f72beb1d96ca19821576e30c5f4a
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-13 14:38:08 -0600

    feat(Root): Adding SignIn component to login page

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn } from "../ui/login/sign-in";
+
+
+export default function Page(){
+    return(
+        <div className="flex justify-center content-center m-20 p-20 h-full ">
+            <SignIn/>
+        </div>
+    );
+}
