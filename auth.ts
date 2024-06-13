8610a0b0a67f1cda362f73891d1de473ba52d6a5
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-13 14:02:26 -0600

    feat(Root): Adding authentication functionality with Auth.js
    Creating auth.ts and route.ts to configure OAth authentication

---
+++ auth.ts
@@ -0,0 +1,62 @@
+import NextAuth, { NextAuthConfig, Session, User } from "next-auth"
+import GitHub from "next-auth/providers/github"
+import Google from "next-auth/providers/google"
+import { JWT } from "next-auth/jwt"
+import { createUser } from "./app/lib/actions";
+import { use } from "react";
+
+
+interface CustomToken extends JWT {
+    id?: string;
+  }
+  
+  interface CustomSession extends Session {
+    user: {
+      id?: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+  }
+
+//import PostgresAdapter from "@auth/pg-adapter"
+
+//import { Pool } from "pg"
+
+export const BASE_PATH = "api/auth";
+
+/*const pool = new Pool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+})
+ */
+
+const authConfig: NextAuthConfig = {
+    providers:[Google, GitHub],
+    callbacks: {
+        async jwt({ token, user }): Promise<CustomToken> {
+          if (user) { // User is available during sign-in
+            token.id = user.id as string;
+            await createUser(user.name as string, user.email as string, user.image as string);
+          }
+          return token;
+        },
+        async session({ session, token }): Promise<CustomSession> {
+          session.user.id = token.id as string;
+          return session;
+        },
+      },
+    
+} 
+
+
+//basePath: BASE_PATH,
+    //secret: process.env.AUTH_SECRET,
+    //adapter: PostgresAdapter(pool),
+
+export const { handlers, signIn, signOut, auth} = NextAuth(authConfig)
