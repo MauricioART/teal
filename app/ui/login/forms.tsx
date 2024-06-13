366886da06a3b03247ff6cc23f33b016a107def0
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-13 14:03:27 -0600

    feat(Root): Adding form components for logIn, logOut

---
+++ app/ui/login/forms.tsx
@@ -0,0 +1,19 @@
+import { authenticate } from "@/app/lib/actions";
+
+export  function LoginForm(){
+
+    return(
+        <div className="flex justify-center mx-10 my-6 p-10">
+            <form action={authenticate} className="flex flex-col justify-center">
+                <div>
+                    <input type="text" name="email" placeholder="Email" id="email" required />
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Password" id="password" required/>
+                </div>
+                <button type="submit"> Log In</button>
+            </form>
+
+        </div>
+    );
+} 
---
+++ app/ui/login/sign-in.tsx
@@ -0,0 +1,25 @@
+
+import { signIn } from "@/auth"
+ 
+export function SignIn() {
+  return (
+    <div className="flex flex-col justify-center border p-5 border-slate-900">
+    <form
+      action={async () => {
+        "use server"
+        await signIn("github",{ redirectTo: "/learn" })
+      }} className=" bg-blue-500 m-2 p-2"
+    >
+      <button type="submit">Signin with GitHub</button>
+    </form>
+    <form
+    action={async () => {
+      "use server"
+      await signIn("google",{ redirectTo: "/learn" })
+    }} className=" bg-gray-200 m-2 p-2"
+  >
+    <button type="submit">Signin with Google</button>
+  </form>
+  </div>
+  )
+} 
