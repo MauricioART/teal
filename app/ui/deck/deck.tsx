ae3ae5acc27bb95256ccf60dc0956563a4597625
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-13 13:41:47 -0600

    feat(Root): Updating Deck component to display a diferent deck icon in case it does not have a cover

--- app/ui/deck/deck.tsx
+++ app/ui/deck/deck.tsx
@@ -8,13 +8,28 @@ interface deckProps{
 
 export default async function Deck(props: deckProps){
     const deck = props.deck;
-    const numberOfCards = await fetchNumberOfCards(deck.deck_id);
-    return (
-        <div className="flex flex-col items-center m-5">
-            <DeckIcon />
-            <p>{deck.name}</p>
-            <p>{numberOfCards} Cards</p>
-            <p>Score: {deck.score}   Used: {deck.used}</p>
-        </div>
-    );
+    if (deck.coverImage != null){
+        const url = URL.createObjectURL(deck.coverImage);
+        console.log(url);
+        return (
+            <div className="flex flex-col items-center m-5">
+                <DeckIcon imageUrl={url}/>
+                <p>{deck.name}</p>
+                <p>{0} Cards</p>
+                <p>Score: {deck.score}   Used: {deck.used}</p>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className="flex flex-col items-center m-5">
+                <DeckIcon />
+                <p>{deck.name}</p>
+                <p>{0} Cards</p>
+                <p>Score: {deck.score}   Used: {deck.used}</p>
+            </div>
+        );
+    }
+    //const numberOfCards = await fetchNumberOfCards(deck.deck_id);
+   
 }
