a4a2257f2769cc7500c0fe81fa67578284e2fcf9
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-15 15:44:34 -0600

    feat(Root): Abording errors due to definition data types.

--- app/ui/learn/play.tsx
+++ app/ui/learn/play.tsx
@@ -11,11 +11,11 @@ interface playProps{
 
 export default function Play( props : playProps){
     const cards: cardType[] = [
-        { question: "What is the capital of France?", answer: 0, options: ["Paris", "Berlin", "Rome", "Madrid"], cardType: 1, priority: 1 },
-        { question: "Is the earth round?", answer: 0, options: ["True", "False"], cardType: 0, priority: 3 },
-        { question: "What is 2 + 2?", answer: 1, options: ["3", "4", "5", "6"], cardType: 1, priority: 2 },
-        { question: "What is the chemical symbol for water?", answer: 0, options: ["H2O", "CO2", "O2", "H2"], cardType: 1, priority: 4 },
-        { question: "Solve the equation: x + 3 = 5. What is x?", answer: 1, options: ["1", "2", "3", "4"], cardType: 1, priority: 5 }
+        { question: "What is the capital of France?", answer: 0, options: ["Paris", "Berlin", "Rome", "Madrid"], cardType: 1},
+        { question: "Is the earth round?", answer: 0, options: ["True", "False"], cardType: 0 },
+        { question: "What is 2 + 2?", answer: 1, options: ["3", "4", "5", "6"], cardType: 1},
+        { question: "What is the chemical symbol for water?", answer: 0, options: ["H2O", "CO2", "O2", "H2"], cardType: 1 },
+        { question: "Solve the equation: x + 3 = 5. What is x?", answer: 1, options: ["1", "2", "3", "4"], cardType: 1 }
       ];
       const decks: deckType[] = [];
     
