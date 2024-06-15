640e0043251976cb6297d3e646a8aeecc849e123
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-15 16:05:20 -0600

    feat(Root): Changing login layout style

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,10 @@ import { SignIn } from "../ui/login/sign-in";
 
 export default function Page(){
     return(
-        <div className="flex justify-center content-center m-20 p-20 h-full ">
-            <SignIn/>
-        </div>
+        <main>
+            <div className="flex justify-center items-center h-screen">
+                <SignIn />
+            </div>
+        </main>
     );
 }
