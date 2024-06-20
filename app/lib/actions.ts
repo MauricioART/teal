8a70f4b1cb891817810a9b3f413efb778ce07aa9
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-20 13:57:00 -0600

    feat(Root): Adding createCard() action to actions.tsx for the insertion of new cards to the DB and i

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,22 +5,29 @@ import {sql} from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-
+import { Card } from '@/app/lib/definitions';
 
 const DeckFormSchema = z.object({
     name: z.string(),
     description: z.string(),
+    owner_id: z.string().uuid()
   });
 
 const UserFormSchema = z.object({
   nickname: z.string(),
-  email: z.string(),
+  email: z.string().email(),
   pictureProfile: z.string(),
-  password: z.string(),
+  password: z.optional(z.string()),
+});
+const CardFormSchema = z.object({
+  deck_id: z.string().uuid(),
+  question: z.string(),
+  answer: z.union([z.string(), z.number()]),
+  options: z.optional(z.array(z.string())),
+  cardType: z.union([z.string(), z.number()])
 });
   
-const CreateUser = UserFormSchema.omit({password: true});
-const CreateDeck = DeckFormSchema.omit({ });
+
  
 
 export async function deleteInvoice ( invoiceId: string , formData: FormData){
@@ -31,25 +38,49 @@ export async function deleteInvoice ( invoiceId: string , formData: FormData){
     
 }
 
-export async function createCard(){
-
+export async function createCard(deckId: string, card: Card){
+  const {deck_id, question, answer, options, cardType} = CardFormSchema.parse({
+    deck_id : deckId,
+    question: card.question,
+    answer: card.answer,
+    options: card.options,
+    cardType: card.cardType
+  });
+  if (options != undefined){
+    let optionsStr = "{";
+    options.map((option,index)=>{
+      if (index < options.length - 1)
+        optionsStr = optionsStr + '"' + option + '"' + ",";
+      else
+        optionsStr = optionsStr + '"' + option + '"' + "}";
+    console.log(optionsStr);
+    });
+    const response = await sql`INSERT INTO cards (deck_id,question,answer,options,card_type)
+                              VALUES (${deck_id},${question},${answer},${optionsStr},${cardType})`;
+  }else{
+    const response = await sql`INSERT INTO cards (deck_id,question,answer,card_type)
+                              VALUES (${deck_id},${question},${answer},${cardType})`;
+  }
+  revalidatePath('/learn/decks');
+  redirect('/learn/decks');
 }
 
-export async function createDeck(formData : FormData, owner_id: string){
-    const { name, description, } = CreateDeck.parse({
+export async function createDeck(formData : FormData){
+    const { name, description, owner_id, } = DeckFormSchema.parse({
         name: formData.get('name'),
         description: formData.get('description'),
+        owner_id: formData.get('owner_id')
       });
       const image = formData.get('image');
-      if (image) {
+      if (name && description && owner_id) {
         // Convert File to Blob (although File is already a Blob)
-        const imageBlob = new Blob([image], { type: "file" });
-        console.log(image);
+        //const imageBlob = new Blob([image], { type: "file" });
+        //console.log(image);
         const response = await sql`INSERT INTO decks (name, owner_id, description, score, used)
-        VALUES (${name},${owner_id},${description},${0},${0})`;
-        console.log(response);
+        VALUES (${name},${owner_id},${description},${0},${0}) RETURNING deck_id`;
+        return response.rows[0].deck_id;
       }
-
+      return null;
 }
 
 export async function updateDeckName(){
@@ -76,11 +107,12 @@ export async function logOut(){
 
 }
 
-export async function createUser(name: string, mail: string, imageURL: string){
-  const { nickname, email, pictureProfile} = CreateUser.parse({
+export async function createUser(name: string, mail: string, imageURL: string, passwd?: string){
+  const { nickname, email, pictureProfile, password} = UserFormSchema.parse({
     nickname: name,
     email: mail,
-    pictureProfile: imageURL
+    pictureProfile: imageURL,
+    password: passwd
   });
   const newUserId = await sql`INSERT INTO users (nickname, email, profile_picture_url) 
                     VALUES (${nickname},${email},${pictureProfile}) RETURNING user_id`;
