8d896e9cb3fb4c1980c7594f8ff51ba1baf440d5
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-20 14:12:35 -0600

    feat(Root): Adding basic functionality to update cards.

--- app/ui/deck/card-collection.tsx
+++ app/ui/deck/card-collection.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Card } from "@/app/lib/definitions";
 import Link from "next/link";
 import AddCardIcon from "./add-card-icon";
-import CardIcon from "./add-card-icon";
+import CardIcon from "./card-icon";
+import Button from "../button";
 
 interface cardsProps{
     add: boolean;
@@ -11,19 +13,28 @@ interface cardsProps{
 
 export default function CardCollection(props: cardsProps){
     let cards = props.cards;
+
+    function handleClick(cardId: number){
+
+    }
     
     return(
         <div className=" flex flex-wrap">
 
             {cards.map((card, index) => {
+                if (props.add) {
                 return(
-                <Link key={index} href={`/learn/decks/${props.deck_id}/edit/${index}/edit`}>
-                    <CardIcon />
-                </Link>);
+                    <button key={index} onClick={()=>{handleClick(index)}}><CardIcon key={index} id={`${index}`}/></button>);
+                }
+                else{
+                    return (
+                        <h1>Hi</h1>
+                    );
+                }
             })}
             {  props.add &&
             <div className="flex flex-col items-center m-10"> 
-                <Link href={`/learn/decks/${props.deck_id}/new`}>
+                <Link key={1} href={`/learn/decks/${props.deck_id}/new`}>
                     <AddCardIcon />
                 </Link>
             </div>}
