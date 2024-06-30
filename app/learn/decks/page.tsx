e8ec526b30093a065cdc271c0e47b3bbd45005d0
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-06-30 10:05:11 -0600

    feat(Root): Adding title to the page.

--- app/learn/decks/page.tsx
+++ app/learn/decks/page.tsx
@@ -5,19 +5,22 @@ import { auth } from "@/auth";
 
 
 export default async function Page (){
-    const session = await auth()
+    //const session = await auth()
     
-    if (!session?.user) return null;
+    //if (!session?.user) return null;
     
     let myDecks: Deck[] = [];
 
-    if (session.user.id != undefined){
-        console.log(session.user.id);
-        myDecks = await fetchDecks(session.user.id);
-    }
+    //if (session.user.id != undefined){
+    //    console.log(session.user.id);
+    //    myDecks = await fetchDecks(session.user.id);
+    //}
 
     return (
-        <div className=" mx-10">
+        <div className="flex flex-col mx-10">
+            <div className="flex justify-center">
+                <h1 className="">My Decks</h1>
+            </div>
             <DeckCollection decks={myDecks}/>
         </div>
     );
