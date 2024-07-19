8184dcb02203d50e96240b2d290bae3381fdd300
ArturoAR <43114558+MauricioART@users.noreply.github.com>
2024-07-19 15:53:24 -0600

    feat(Root): Adding Timer component for Timer Play Mode.

--- app/ui/learn/timer.tsx
+++ app/ui/learn/timer.tsx
@@ -2,13 +2,12 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Button, Box, Typography } from '@mui/material';
 
 interface TimerProps {
-    start: boolean;
-    time: number;
-    setTime: React.Dispatch<React.SetStateAction<number>>;
-  }
+  start: boolean;
+  time: number;
+  setTime: React.Dispatch<React.SetStateAction<number>>;
+}
 
-  
-const Timer: React.FC<TimerProps> = ({start,time,setTime}) => {
+const Timer: React.FC<TimerProps> = ({ start, time, setTime }) => {
   const [isRunning, setIsRunning] = useState(start);
   const timerRef = useRef<NodeJS.Timeout | null>(null);
 
@@ -21,10 +20,24 @@ const Timer: React.FC<TimerProps> = ({start,time,setTime}) => {
   };
 
   useEffect(() => {
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        if (timerRef.current) {
+          clearInterval(timerRef.current);
+          timerRef.current = null;
+        }
+      } else if (isRunning) {
+        timerRef.current = setInterval(() => {
+          setTime(prevTime => prevTime - 1);
+        }, 1000);
+      }
+    };
+    
     if (isRunning && time > 0) {
       timerRef.current = setInterval(() => {
         setTime(prevTime => prevTime - 1);
       }, 1000);
+      document.addEventListener('visibilitychange', handleVisibilityChange);
     } else if (time === 0) {
       setIsRunning(false);
       if (timerRef.current) {
@@ -35,38 +48,20 @@ const Timer: React.FC<TimerProps> = ({start,time,setTime}) => {
       clearInterval(timerRef.current);
       timerRef.current = null;
     }
+
     return () => {
       if (timerRef.current) {
         clearInterval(timerRef.current);
       }
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, [isRunning, time]);
 
-
-  const handleStart = () => setIsRunning(true);
-  const handleStop = () => setIsRunning(false);
-  const handleReset = () => {
-    setIsRunning(false);
-    setTime(0);
-  };
-
   return (
     <div className='flex flex-col justify-center items-center'>
-        <Typography variant="h4" className='border-2 border-dotted border-cyan-700 p-5 rounded-full col-span-2 w-fit'>{formatTime(time)}</Typography>
-      {/*<Box mt={2}>
-        {!isRunning ? (
-          <Button variant="contained" color="primary" onClick={handleStart}>
-            Start
-          </Button>
-        ) : (
-          <Button variant="contained" color="secondary" onClick={handleStop}>
-            Stop
-          </Button>
-        )}
-        <Button variant="contained" onClick={handleReset} style={{ marginLeft: '10px' }}>
-          Reset
-        </Button>
-      </Box>*/}
+      <Typography variant="h4" className='border-2 border-dotted border-cyan-700 p-5 rounded-full col-span-2 w-fit'>
+        {formatTime(time)}
+      </Typography>
     </div>
   );
 };
